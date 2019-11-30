f0b72b62993652c6b1fc5f5696f25bafbaa46b21
win11905 <donothavemoney@gmail.com>
2019-11-30 19:29:05 +0700

    add

--- baseline.js
+++ baseline.js
@@ -1,69 +1,92 @@
 let map = require("./global").map;
 
+const transformdi = (res, pos) => {
+  let x = 0,
+    y = 0;
+  if (
+    typeof pos.north === "number" &&
+    typeof pos.east === "number" &&
+    pos.south === undefined &&
+    pos.west === undefined
+  ) {
+    x = pos.east;
+    y = pos.north;
+  } else if (
+    pos.north === undefined &&
+    typeof pos.east === "number" &&
+    typeof pos.south === "number" &&
+    pos.west === undefined
+  ) {
+    x = pos.east;
+    y = -pos.south;
+  } else if (
+    pos.north === undefined &&
+    pos.east === undefined &&
+    typeof pos.south === "number" &&
+    typeof pos.west === "number"
+  ) {
+    x = -pos.west;
+    y = -pos.south;
+  } else if (
+    typeof pos.north === "number" &&
+    pos.east === undefined &&
+    pos.south === undefined &&
+    typeof pos.west === "number"
+  ) {
+    x = -pos.west;
+    y = pos.north;
+  } else {
+    res.statusMessage = "Request was ill-formed";
+    res.status(400).end();
+  }
+  return [x, y];
+};
+
+const transform = (res, pos) => {
+  if (pos === undefined) {
+    res.statusMessage = "Request was ill-formed";
+    res.status(400).end();
+  } else {
+    let x, y;
+    if (typeof pos === "string") {
+      if (/^robot#([1-9][0-9]*)$/.test(pos)) {
+        if (!map.has(pos)) {
+          res.statusMessage = "Insufficient data to compute the result";
+          res.status(424).end();
+        }
+        x = map.get(pos).x;
+        y = map.get(pos).y;
+      } else {
+        res.statusMessage = "Request was ill-formed";
+        res.status(400).end();
+      }
+    } else {
+      if (typeof pos.x === "number" && typeof pos.y === "number") {
+        x = pos.x;
+        y = pos.y;
+      } else {
+        const z = transformdi(res, pos);
+        (x = z[0]), (y = z[1]);
+      }
+    }
+    return [x, y];
+  }
+};
+
 const distance = (req, res) => {
   // Works
   const first_pos = req.body.first_pos;
   const second_pos = req.body.second_pos;
   const metric = req.body.metric;
-
   if (first_pos === undefined || second_pos === undefined) {
     res.statusMessage = "Request was ill-formed";
     res.status(400).end();
   } else {
-    let x1, y1, x2, y2;
-    // Assign x1, y1, to first_pos
-    if (typeof first_pos === "string") {
-      if (/^robot#([1-9][0-9]*)$/.test(first_pos)) {
-        if (!map.has(first_pos)) {
-          res.statusMessage = "Insufficient data to compute the result";
-          res.status(424).end();
-        }
-        x1 = map.get(first_pos).x;
-        y1 = map.get(first_pos).y;
-      } else {
-        res.statusMessage = "Request was ill-formed";
-        res.status(400).end();
-      }
-    } else {
-      if (
-        typeof req.body.first_pos.x === "number" &&
-        typeof req.body.first_pos.y === "number"
-      ) {
-        x1 = req.body.first_pos.x;
-        y1 = req.body.first_pos.y;
-      } else {
-        res.statusMessage = "Request was ill-formed";
-        res.status(400).end();
-      }
-    }
-    // Assign x2, y2, to first_pos
-    if (typeof second_pos === "string") {
-      if (/^robot#([1-9][0-9]*)$/.test(second_pos)) {
-        if (!map.has(second_pos)) {
-          res.statusMessage = "Insufficient data to compute the result";
-          res.status(424).end();
-        }
-        x2 = map.get(second_pos).x;
-        y2 = map.get(second_pos).y;
-      } else {
-        res.statusMessage = "Request was ill-formed";
-        res.status(400).end();
-      }
-    } else {
-      if (
-        typeof req.body.second_pos.x === "number" &&
-        typeof req.body.second_pos.y === "number"
-      ) {
-        x2 = req.body.second_pos.x;
-        y2 = req.body.second_pos.y;
-      } else {
-        res.statusMessage = "Request was ill-formed";
-        res.status(400).end();
-      }
-    }
+    let a = transform(res, first_pos);
+    let b = transform(res, second_pos);
 
-    const dx = Math.abs(x1 - x2);
-    const dy = Math.abs(y1 - y2);
+    const dx = Math.abs(a[0] - b[0]);
+    const dy = Math.abs(a[1] - b[1]);
 
     res.statusMessage = "Distance is computed";
     if (metric === "manhattan") {
@@ -80,19 +103,21 @@ const distance = (req, res) => {
 
 const setposition = (req, res) => {
   // Works
-  if (
-    /^([1-9][0-9]*)$/.test(req.params.id) === false ||
-    req.body.position === undefined ||
-    req.body.position.x == undefined ||
-    req.body.position.y === undefined ||
-    typeof req.body.position.x !== "number" ||
-    typeof req.body.position.y !== "number"
-  ) {
+  let z = {};
+  if (/^([1-9][0-9]*)$/.test(req.params.id) === false) {
     res.statusMessage = "Request was ill-formed";
     res.status(400).end();
   }
+  if (
+    typeof req.body.position.x !== "number" ||
+    typeof req.body.position.y !== "number"
+  ) {
+    let now = transformdi(res, req.body.position);
+    (z.x = now[0]), (z.y = now[1]);
+  }
   const ss = "robot#" + req.params.id;
-  map.set(ss, req.body.position);
+
+  map.set(ss, z);
   res.statusMessage = "Current position of the robot is updated";
   res.status(204).end();
 };
