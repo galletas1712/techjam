319451f5861e0fd9ffe533fe69421fdeec5d5838
Schwinn Saereesitthipitak <winaichan263@gmail.com>
2019-11-30 16:16:35 +0700

    Modified baseline

--- baseline.js
+++ baseline.js
@@ -1,14 +1,24 @@
 const distance = (req, res) => {
     const first_pos = req.body.first_pos;
     const second_pos = req.body.second_pos;
-    if(typeof first_pos.x !== 'number' || typeof first_pos.y !== 'number' || typeof second_pos.x !== 'number' || typeof second_pos.y !== 'number') {
+    const metric = req.body.metric;
+
+    if(first_pos.x === undefined || first_pos.y === undefined || second_pos.x === undefined || second_pos.y === undefined || metric === undefined) {
+        res.statusMessage = "Insufficient data to compute the result";
+        res.status(424).end();
+    } else if(typeof first_pos.x !== 'number' || typeof first_pos.y !== 'number' || typeof second_pos.x !== 'number' || typeof second_pos.y !== 'number' || (metric !== "euclidean" && metric !== "manhattan")) {
         res.statusMessage = "Request was ill-formed";
         res.status(400).end();
     } else {
-        const dx = first_pos.x - second_pos.x;
-        const dy = first_pos.y - second_pos.y;
+        const dx = Math.abs(first_pos.x - second_pos.x);
+        const dy = Math.abs(first_pos.y - second_pos.y);
+
         res.statusMessage = "Distance is computed";
-        res.send({"distance" : Math.sqrt(dx * dx + dy * dy)});
+        if(metric === "euclidean") {
+            res.send({"distance" : Math.sqrt(dx * dx + dy * dy)});
+        } else {
+            res.send({"distance" : dx + dy});
+        }
     }
 };
 
